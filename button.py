a2bd8c1d1d919b5116ae95a678bea8a8be4ef6c2
Duve3 <Duv3tabest@gmail.com>
2023-06-02 18:24:59 -0700

    made button.py more readable :)

--- button.py
+++ button.py
@@ -1,29 +1,30 @@
 import pygame
 
-class button():
-  def __init__(self, color, x,y,width,height, text=''):
-    self.color = color
-    self.x = x
-    self.y = y
-    self.width = width
-    self.height = height
-    self.words = text
 
-  def draw(self,win,outline=None):
-    #Call this method to draw the button on the screen
-    if outline:
-        pygame.draw.rect(win, outline, (self.x-2,self.y-2,self.width+4,self.height+4),0)
-        
-    pygame.draw.rect(win, self.color, (self.x,self.y,self.width,self.height),0)
-    
-    if self.words != '':
-        font = pygame.font.SysFont('', 60)#idk what text to do
-        text = font.render(self.words, 1, (0,0,0))
-        win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+class Button:
+    def __init__(self, color, x, y, width, height, text=''):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.words = text
 
-  def isOver(self, pos):
-    if pos[0] > self.x and pos[0] < self.x + self.width:
-        if pos[1] > self.y and pos[1] < self.y + self.height:
-            return True
-        
-    return False
+    def draw(self, win, outline=None):
+        # Call this method to draw the button on the screen
+        if outline:
+            pygame.draw.rect(win, outline, (self.x - 2, self.y - 2, self.width + 4, self.height + 4), 0)
+
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height), 0)
+
+        if self.words != '':
+            font = pygame.font.SysFont('', 60)  # idk what text to do
+            text = font.render(self.words, True, (0, 0, 0))
+            win.blit(text, (self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
+
+    def isOver(self, pos):
+        if self.x < pos[0] < self.x + self.width:
+            if self.y < pos[1] < self.y + self.height:
+                return True
+
+        return False
