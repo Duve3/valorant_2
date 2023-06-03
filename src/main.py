d50290fd0c4b82c4a7a37245d8368aae5cac6e8c
Duve3 <Duv3tabest@gmail.com>
2023-06-02 23:22:04 -0700

    Added Assets, fixed some resolution issues, and fade in loading screen yeah yeah

--- src/main.py
+++ src/main.py
@@ -5,12 +5,12 @@ from main_menu import MainMenu
 def main():  # the main game
     pygame.init()
 
-    RES = [500, 500]
+    RES = [1280, 720]
     window = pygame.display.set_mode(RES)
     fpsClock = pygame.time.Clock()
 
     # probably run mainMenu here but im not done with it
-    mm = MainMenu(window=window, fpsClock=fpsClock)
+    mm = MainMenu(window=window, fpsClock=fpsClock, width=RES[0], height=RES[1])
     MainMenu.run(mm)
 
 
--- src/main_menu.py
+++ src/main_menu.py
@@ -1,4 +1,6 @@
 import pygame
+import constants
+from util import createFont
 
 
 class MainMenu:
@@ -6,9 +8,18 @@ class MainMenu:
     Main Menu
     """
 
-    def __init__(self, window: pygame.Surface, fpsClock: pygame.time.Clock):
+    def __init__(self, window: pygame.Surface, fpsClock: pygame.time.Clock, width: int, height: int):
+        self.res = (width, height)
         self.display: pygame.Surface = window
         self.fpsClock: pygame.time.Clock = fpsClock
+        self.testRectangle = pygame.Rect(15, 15, 50, 50)
+
+        # LOADING SCREEN:
+        self.loadingImage = pygame.image.load("../assets/valorantLoading.png").convert_alpha()
+        self.loadingImage = pygame.transform.smoothscale(self.loadingImage, [width, height])
+        self.loadingImageALPHA = self.loadingImage.get_alpha()
+        self.userInput = False
+        self.FONTwaitingForUserInput = createFont(constants.white, 40, fontLocation="../assets/CourierPrimeCode-Regular.ttf")
 
     def run(self):
         while True:
@@ -17,7 +28,19 @@ class MainMenu:
                 if event.type == pygame.QUIT:
                     return  # exit but better
 
-            pygame.draw.rect(self.display, )
+                if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:  # detect player input
+                    self.userInput = True
+
+            self.display.fill(constants.black)
+
+            self.display.blit(self.loadingImage, [0, 0])
+            if self.userInput:
+                if self.loadingImageALPHA > 0:
+                    self.loadingImageALPHA -= 2
+                    self.loadingImage.set_alpha(self.loadingImageALPHA)
+            else:
+                self.FONTwaitingForUserInput.render_to(self.display, [self.res[0]//2 - 250, 50], "Waiting for User Input...")
+
 
             pygame.display.flip()
 
