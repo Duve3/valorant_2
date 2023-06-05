d7c8d390e59c4255c72e243cc5990df873908962
twoshotsonekill <135389624+twoshotsonekill@users.noreply.github.com>
2023-06-04 20:34:50 -0700

    Update player.py

--- src/player.py
+++ src/player.py
@@ -1,6 +1,14 @@
-class Player:
-    def __init__(self, x, y, pid):
-        self.id = pid
-        self.x = x
-        self.y = y
-        self.models = []  # TODO: add models to player + assets
+class world:
+    def __init__(self, blocklist):
+        self.blocklist = blocklist
+        self.x = 400
+        self.y = 400
+    
+    def checkup(self):
+        for block in self.blocklist:
+            print("drawn")
+            if self.x + 500 > block["x"] and self.x - 500 < block["x"] and self.y + 500 > block["y"] and self.y - 500 < block["y"]:
+                pygame.draw.rect(block["screen"], block["color"], pygame.Rect(block["x"]-self.x, block["y"]-self.y, block["width"], block["height"]))
+    
+    def movement(self, dx, dy):
+        self.x += dx
