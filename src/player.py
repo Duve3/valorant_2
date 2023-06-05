6c465d2c3b69d743500e1ad8e298aa30b604761f
twoshotsonekill <135389624+twoshotsonekill@users.noreply.github.com>
2023-06-04 20:37:31 -0700

    Update player.py

--- src/player.py
+++ src/player.py
@@ -2,13 +2,13 @@ class world:
     def __init__(self, blocklist):
         self.blocklist = blocklist
         self.x = 400
-        self.y = 400
+        self.y = 400 # #location in the world
     
     def checkup(self):
         for block in self.blocklist:
-            print("drawn")
-            if self.x + 500 > block["x"] and self.x - 500 < block["x"] and self.y + 500 > block["y"] and self.y - 500 < block["y"]:
+            if self.x + 500 > block["x"] and self.x - 500 < block["x"] and self.y + 500 > block["y"] and self.y - 500 < block["y"]: #onley draws things that you see
                 pygame.draw.rect(block["screen"], block["color"], pygame.Rect(block["x"]-self.x, block["y"]-self.y, block["width"], block["height"]))
     
     def movement(self, dx, dy):
         self.x += dx
+        self.y += dy
