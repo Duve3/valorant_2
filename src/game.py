63f182f72790719780406cb79576baf589ae9442
twoshotsonekill <135389624+twoshotsonekill@users.noreply.github.com>
2023-06-28 20:04:51 -0700

    Create game.py

---
+++ src/game.py
@@ -0,0 +1,107 @@
+import pygame
+from pygame.math import Vector2
+pygame.init()
+bullets = []#bullets.append(bullet(x,y,damage))
+size = 800
+screen = pygame.display.set_mode([size, size])
+fpsClock = pygame.time.Clock()
+reloading = False
+rcountdown = 0
+class Bullet:
+    def __init__(self, pos, target,damage):
+        self.damage = damage
+        self.speed = 5
+        self.heading = None
+        self.is_active = False
+        direction = target - pos
+        self.position = Vector2(pos) 
+        radius, angle = direction.as_polar()
+        self.velocity = direction.normalize() * 11
+        self.timealive = 1000
+    def update(self):
+        self.position += self.velocity
+        self.timealive -= 1
+        if self.timealive <= 0:
+            self.kill()
+
+    def draw(self, screen):
+        #A little bit silly
+        pygame.draw.rect(screen,(0, 0, 0),(self.x, self.y, 10, 10))
+    def kill(self):
+        bullets.pop(0)#Would not work with multi player or would need to make a list for each player
+    
+
+        
+
+        
+class world:
+    def __init__(self, blocklist):
+        self.count = 0
+        self.blocklist = blocklist
+        self.x = 400
+        self.y = 400
+        self.bulletcount = 25
+    
+    def checkup(self):
+        for block in self.blocklist:
+            if self.x + size > block["x"] and self.x - size < block["x"] and self.y + size > block["y"] and self.y - size < block["y"]:
+                pygame.draw.rect(block["screen"], block["color"], pygame.Rect(block["x"]+size/2-self.x, block["y"]+size/2-self.y, block["width"], block["height"]))
+                self.count += 1
+    def bulletsdraw(self):
+        for bullet in bullets:
+            pygame.draw.rect(screen, (200, 0, 0), pygame.Rect(bullet.position.x+size/2-self.x, bullet.position.y+size/2-self.y, 10, 10))
+            bullet.update()
+
+            
+    def drawself(self):
+        pygame.draw.rect(screen, (0, 200, 0), pygame.Rect(size/2, size/2, 20, 20))
+    def movement(self, dx, dy):
+        self.x += dx
+        self.y += dy
+    def shoot(self,damage):
+        x, y = pygame.mouse.get_pos()
+        target_x = x - size / 2 + self.x
+        target_y = y - size / 2 + self.y
+        bullets.append(Bullet(Vector2(self.x,self.y),Vector2(target_x,target_y),damage))#SELF.x,self.y is not right to the spot of the player as the calulation is made without the self.x,self.y
+
+
+bind = world([{"screen": screen, "color": (0, 0, 200), "x": 400, "y": 400, "width": 20, "height": 20}])
+
+pygame.display.set_caption("2D Space")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                bind.movement(-10, 0)
+            elif event.key == pygame.K_RIGHT:
+                bind.movement(10, 0)
+            elif event.key == pygame.K_UP:
+                bind.movement(0, -10)
+            elif event.key == pygame.K_DOWN:
+                bind.movement(0, 10)
+    if pygame.mouse.get_pressed()[0]:
+            if bind.bulletcount > 0 and not reloading:
+                bind.shoot(10)
+                bind.bulletcount -= 1
+            elif not reloading:
+                print()
+                reloading = True
+                rcountdown = 120
+                bind.bulletcount = 25
+    
+    rcountdown -= 1
+    if rcountdown < 0:
+            reloading = False
+    screen.fill((0,0,0))
+    bind.checkup()
+    bind.drawself()
+    bind.bulletsdraw()
+    pygame.display.flip()
+    fpsClock.tick(60)
+    print(len(bullets))
+
+pygame.quit()
